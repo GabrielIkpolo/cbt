20a897d291df572af565903837af0916c9adc17b
GabrielIkpolo <contact.gikps@gmail.com>
2024-03-01 09:19:26 +0100

    Fixed hanging footer on small devices

--- ui/src/components/HeaderNav.jsx
+++ ui/src/components/HeaderNav.jsx
@@ -10,9 +10,9 @@ const HeaderNav = () => {
         <img className="logo" src={logo} alt="log" />
         <nav className="nav">
             <ul className="navListParent">
-                <li className="listItems"><NavLink className="navLink">Home</NavLink></li>
-                <li className="listItems"><NavLink className="navLink">Register</NavLink></li>
-                <li className="listItems"><NavLink className="navLink">Feedback</NavLink></li>
+                <li className="listItems"><NavLink to="/" className="navLink">Home</NavLink></li>
+                <li className="listItems"><NavLink to="register" className="navLink">Register</NavLink></li>
+                <li className="listItems"><NavLink to="feedback" className="navLink">Feedback</NavLink></li>
             </ul>
         </nav>
         <button className="loginButton"> Login</button>
